25550b3a2ab4f164bdd2749ad6f5c1aa8a4b6a24
jfrank03 <jfrank@vassar.edu>
2025-01-17 16:24:22 -0500

    Updated autoInc with comments explaining functionality  Please enter the commit message for your changes. Lines starting

--- public/js/autoInc.js
+++ public/js/autoInc.js
@@ -1,25 +1,34 @@
-let intervalId = null;
-const incrementInput = document.getElementById('toggleIncrement');
-let increment;
-let interval_ms = 60000;
+/* 
+* Manages a toggle switch and an input field. When toggled on, the program will
+* increment the time automatically by x in-game minutes every 1 real-world minute, where
+* x is the number in the input field.
+*/ 
 
+//This is the interval, which will be created and execute every interval_ms milliseconds (Currently 1 minute).
+let intervalId = null; 
+let interval_ms = 60000;    //Time interval between increments in ms. Currently 1 minute real-world.
+const incrementInput = document.getElementById('toggleIncrement'); //Field to read the increment from.
+let increment;  //Size of in-game time increment, as input by the user.
+
+//Listen for a change in the input field.
 incrementInput.addEventListener('change', function(){
+    //If there is a value input, 
     if(incrementInput.value != ''){
         increment = parseInt(incrementInput.value);
+        //Reset/create the interval with new increment.
         intervalId = setInterval(() => {
-            incTime('minutes', increment);
+            //Calls increment from Clock.
+            incTime('minutes', increment);  //I should make a Clock object. 
         }, interval_ms);
     }  
+    // If the input field is empty, remove the interval. That way, it stops incrementing.
     else {
         clearInterval(intervalId);
         intervalId = null;
     }
 });
 
+//Listen for change in increment input.
 document.getElementById('toggleIncrement').addEventListener('change', function(){
     increment = parseInt(document.getElementById('toggleIncrement').value);
-});
-
-document.getElementById('toggleTimer').addEventListener('change', function(){
-    interval  = parseInt(document.getElementById('toggleTimer').value);
 });
